29b29d06e95310622a4e792361d9b1ea68b08e99
sojungpp <sophie2458@naver.com>
2023-05-16 21:26:56 +0900

    #1 [feat] 메인 자산 get API 구현

--- src/main/java/com/sopt/toss/controller/asset/AssetController.java
+++ src/main/java/com/sopt/toss/controller/asset/AssetController.java
@@ -2,6 +2,7 @@ package com.sopt.toss.controller.asset;
 
 import com.sopt.toss.common.dto.ApiResponse;
 import com.sopt.toss.controller.asset.dto.response.AssetResDto;
+import com.sopt.toss.exception.Success;
 import com.sopt.toss.service.AssetService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+import static com.sopt.toss.common.Constant.Constant.AUTHORIZATION;
+
 @RequestMapping("/asset")
 @RequiredArgsConstructor
 @RestController
 public class AssetController {
     private final AssetService assetService;
 
+    /**
+     * [GET] 메인 자산
+     */
     @GetMapping
-    public ApiResponse<List<AssetResDto>> getAssets() {
-        /* TODO */
-        return null;
+    public ApiResponse<List<AssetResDto>> getAssets(HttpServletRequest request) {
+        long userId = Long.parseLong(request.getHeader(AUTHORIZATION));
+        return ApiResponse.success(Success.ASSERT_SUCCESS, assetService.getAssets(userId));
     }
 }
--- src/main/java/com/sopt/toss/repository/AssetRepository.java
+++ src/main/java/com/sopt/toss/repository/AssetRepository.java
@@ -1,4 +1,11 @@
 package com.sopt.toss.repository;
 
-public interface AssetRepository {
+import com.sopt.toss.domain.Asset;
+import com.sopt.toss.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AssetRepository extends JpaRepository<Asset, Long> {
+    List<Asset> findAllByUser(User user);
 }
